a8098db8db155afbca2e24334dbe5ef31b1e3371
Rauulll <nyaokeraul003@gmail.com>
2024-08-17 11:38:27 +0300

    initialize assets

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :chequeit, Chequeit.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "chequeit_dev",
+  database: "chequeit_test",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +19,7 @@ config :chequeit, Chequeit.Repo,
 config :chequeit, ChequeitWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 3000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
