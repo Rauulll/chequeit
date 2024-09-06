6b288abe6ac8a2292699e0a982420c73852d929c
Rauulll <nyaokeraul003@gmail.com>
2024-09-06 17:14:09 +0300

    Update .gitignore and gitguardian pre-commit

--- lib/chequeit_web/controllers/plaid_controller.ex
+++ lib/chequeit_web/controllers/plaid_controller.ex
@@ -28,23 +28,30 @@ defmodule ChequeitWeb.PlaidController do
   end
 
   def create_plaid_link({:ok, %LinkToken.CreateResponse{link_token: _link_token}}, conn) do
-    {:ok, %Plaid.Sandbox.CreatePublicTokenResponse{public_token: public_token}} = Sandbox.create_public_token("ins_1", ["auth"], ApiController.plaid_config)
+    {:ok, %Plaid.Sandbox.CreatePublicTokenResponse{public_token: public_token}} =
+      Sandbox.create_public_token(
+        "ins_1",
+        ["auth"],
+        ApiController.plaid_config
+      )
 
     exchange_token =
       Item.exchange_public_token(public_token, ApiController.plaid_config)
 
     handle_exchange_token(exchange_token, conn)
-    end
+  end
 
   def create_plaid_link({:error, %Plaid.Error{error_code: error}}, conn) do
     render(conn, error: error, layout: false)
   end
 
-  def handle_exchange_token({:ok, %Plaid.Item.ExchangePublicTokenResponse{access_token: access_token, item_id: item_id}}, conn) do
-    conn
-    |> assign(:access_toker, access_token)
-    |> assign(:item_id, item_id)
-    |> render(layout: false)
+  def handle_exchange_token(
+    {:ok, %Plaid.Item.ExchangePublicTokenResponse{access_token: access_token, item_id: item_id}}, conn
+    ) do
+      conn
+      |> assign(:access_token, access_token)
+      |> assign(:item_id, item_id)
+      |> render(layout: false)
   end
 
   def create(_conn, _params) do
